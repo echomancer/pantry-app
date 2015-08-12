bffa8b4150d2d55d593c66d1345348bcc62ed852
Jason Perkins <jasontperkins@gmail.com>
2015-08-12 08:31:50 -0400

    Updating user for friendly id, adding slug to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,28 @@
 class User < ActiveRecord::Base
-  enum role: [:user, :vip, :admin]
-  after_initialize :set_default_role, :if => :new_record?
+	# Adding friendly id and slug candidates
+	extend FriendlyId
+	friendly_id :slug_candidates, use: [:slugged,:finders]
+	enum role: [:user, :vip, :admin]
+	after_initialize :set_default_role, :if => :new_record?
 
-  def set_default_role
-    self.role ||= :user
-  end
+	def set_default_role
+		self.role ||= :user
+	end
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :invitable, :database_authenticatable, :registerable, :confirmable,
+	     :recoverable, :rememberable, :trackable, :validatable
+
+	# Adding function to return role enum for admin pages
+	def role_enum
+		[:user,:vip,:admin]
+	end
+
+	# Try build a slug based on the following fields in increasing order of specifity
+	def slug_candidates
+		[
+			[:name]
+		]
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150812115536) do
+ActiveRecord::Schema.define(version: 20150812122645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -43,6 +56,7 @@ ActiveRecord::Schema.define(version: 20150812115536) do
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.integer  "invitations_count",      default: 0
+    t.string   "slug"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
@@ -50,5 +64,6 @@ ActiveRecord::Schema.define(version: 20150812115536) do
   add_index "users", ["invitations_count"], name: "index_users_on_invitations_count", using: :btree
   add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["slug"], name: "index_users_on_slug", unique: true, using: :btree
 
 end
