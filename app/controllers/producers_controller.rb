5ed6ee0ab870aa073083d1b6b109fe172715b7a0
Jason Perkins <jasontperkins@gmail.com>
2015-08-12 11:01:50 -0400

    Fixing the lack of json for showing producers and stores

--- app/controllers/producers_controller.rb
+++ app/controllers/producers_controller.rb
@@ -1,8 +1,18 @@
 class ProducersController < InheritedResources::Base
 	before_action :authenticate_user!
+	before_action :set_producer, only: [:show, :edit, :update, :destroy]
 
+	def show
+		respond_to do |format|
+			format.html
+			format.json {render json: @producer}
+		end
+	end
 
-	private
+  	private
+  		def set_producer
+  			@producer = Producer.find(params[:id])
+  		end
 
 		def producer_params
 			params.require(:producer).permit(:name)
--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,7 +1,18 @@
 class StoresController < InheritedResources::Base
 	before_action :authenticate_user!
+	before_action :set_store, only: [:show, :edit, :update, :destroy]
+
+	def show
+		respond_to do |format|
+			format.html
+			format.json {render json: @store}
+		end
+	end
 
   	private
+  		def set_store
+  			@store = Store.find(params[:id])
+  		end
 
 	    def store_params
 	      params.require(:store).permit(:name)
